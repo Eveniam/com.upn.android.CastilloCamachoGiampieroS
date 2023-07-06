e50117802effa9d226b6eed59d08c21444e8e9e9
Eveniam <102640726+Eveniam@users.noreply.github.com>
2023-07-06 11:00:46 -0500

    main

---
+++ app/src/main/java/com/example/comupnandroidfinalcastillocamachogiampieros/MostrarCarta.java
@@ -0,0 +1,14 @@
+package com.example.comupnandroidfinalcastillocamachogiampieros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarCarta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_carta);
+    }
+}
