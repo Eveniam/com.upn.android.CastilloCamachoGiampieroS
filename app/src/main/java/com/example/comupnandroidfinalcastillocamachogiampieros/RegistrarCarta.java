6315ecdc86709749c1e958f5a63aae8aa58024fb
Eveniam <102640726+Eveniam@users.noreply.github.com>
2023-07-07 17:54:50 -0500

    main

--- app/src/main/java/com/example/comupnandroidfinalcastillocamachogiampieros/RegistrarCarta.java
+++ app/src/main/java/com/example/comupnandroidfinalcastillocamachogiampieros/RegistrarCarta.java
@@ -181,19 +181,8 @@ public class RegistrarCarta extends AppCompatActivity {
 //
 //    }
 
-        btnFoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                handleOpenCamera();
-            }
-        });
-
-        btnGaleria.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGallery();
-            }
-        });
+        btnFoto.setOnClickListener(view -> CamaraUtils.setUpOpenCamera(RegistrarCarta.this));
+        btnGaleria.setOnClickListener(view -> CamaraUtils.setUpOpenGallery(RegistrarCarta.this));
 
         btnGC.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -244,115 +233,44 @@ public class RegistrarCarta extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == OPEN_CAMERA_REQUEST && resultCode == RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            ivCarta.setImageBitmap(photo);
-
-            imagenBase64 = BitmaptoBase64(photo);
-
-            if (isNetworkConnected()) {
-                base64toLink(imagenBase64);
-
-            }
-
-        }
-        if(requestCode == OPEN_GALLERY_REQUEST && resultCode == RESULT_OK) {
-            Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close(); // close cursor
-
-            Bitmap bitmap = BitmapFactory.decodeFile(picturePath);
-            ivCarta.setImageBitmap(bitmap);
-
-            imagenBase64 = BitmaptoBase64(bitmap);
-
-            if (isNetworkConnected()) {
-                base64toLink(imagenBase64);
-
-            }
-
-//            base64toLink(base64);
-        }
-    }
-
-
-
-    private void handleOpenCamera() {
-        if(checkSelfPermission(android.Manifest.permission.CAMERA)  == PackageManager.PERMISSION_GRANTED)
-        {
-            // abrir camara
-            Log.i("MAIN_APP", "Tiene permisos para abrir la camara");
-            openCamara();
-        } else {
-            // solicitar el permiso
-            Log.i("MAIN_APP", "No tiene permisos para abrir la camara, solicitando");
-            String[] permissions = new String[] {Manifest.permission.CAMERA};
-            requestPermissions(permissions, 1001);
-        }
-    }
-
-    private void openCamara() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, OPEN_CAMERA_REQUEST);
-    }
-    private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, OPEN_GALLERY_REQUEST);
-    }
-
-
-    private String BitmaptoBase64 (Bitmap imagenBitmap){
+        if(resultCode != RESULT_OK) return;
+        Bitmap photo = CamaraUtils.GetBitMap(this, requestCode, data);
+        ivCarta.setImageBitmap(photo);
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        imagenBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        photo.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
         byte[] byteArray = byteArrayOutputStream .toByteArray();
 
-        String Resulbase64 = Base64.encodeToString(byteArray, Base64.DEFAULT);
-        Log.i("APP_MAIN", Resulbase64);
-        return Resulbase64;
-    }
+        String imgBase64 = Base64.encodeToString(byteArray, Base64.DEFAULT);
 
-    private Bitmap Base64toBitmap(String imagenBase64){
-        byte[] decodedBytes = Base64.decode(imagenBase64, Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(decodedBytes,0,decodedBytes.length);
 
-    }
-    private void base64toLink(String base64) {
-        Retrofit retrofit1 = new Retrofit.Builder()
-                .baseUrl("https://demo-upn.bit2bittest.com/")
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://demo-upn.bit2bittest.com/") // revisar
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        CartaService service = retrofit1.create(CartaService.class);
-        Call<CartaService.ImagenResponse> call = service.guardarImage(new CartaService.ImagenToSave(base64));
-        call.enqueue(new Callback<CartaService.ImagenResponse>() {
+
+        ImageService service = retrofit.create(ImageService.class);
+
+        ImageRequest req = new ImageRequest();
+        req.base64Image = imgBase64;
+
+        Call<ImageResult> call = service.convert(req);
+        call.enqueue(new Callback<ImageResult>() {
             @Override
-            public void onResponse(Call<CartaService.ImagenResponse> call, Response<CartaService.ImagenResponse> response) {
-                if (response.isSuccessful()) {
-                    CartaService.ImagenResponse imageResponse = response.body();
-                    Log.i("Respues", response.toString());
-                    urlImage = "https://demo-upn.bit2bittest.com/" + imageResponse.getUrl();
-                    //tvUrlImagenMov.setText(urlImage);
-                    Toast.makeText(getBaseContext(), "Link GENERADO", Toast.LENGTH_SHORT).show();
-                    Log.i("Imagen url:", urlImage);
-
-                } else {
-
-                    Log.e("Error cargar imagen",response.toString());
+            public void onResponse(Call<ImageResult> call, Response<ImageResult> response) {
+                if(response.isSuccessful()) {
+                    urlImage = response.body().url;
+                    Log.i("MAIN_APP", urlImage);
                 }
             }
 
             @Override
-            public void onFailure(Call<CartaService.ImagenResponse> call, Throwable t) {
+            public void onFailure(Call<ImageResult> call, Throwable t) {
 
             }
         });
-
     }
+
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnected();
